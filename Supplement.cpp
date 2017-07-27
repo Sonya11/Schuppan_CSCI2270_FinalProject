9f4afc3d268e154ee4511600a2858aaa39365eac
Sonya <sonya.schuppan@colorado.edu>
2017-07-27 07:14:27 -0600

    minor instruction changes

--- Supplement.cpp
+++ Supplement.cpp
@@ -6,15 +6,17 @@ void header(){
     std::cout<<"WELCOME TO THE VIRTUAL RUBIX CUBE!"<<std::endl;
     std::cout<<""<<std::endl;
     std::cout<<"Note: "<<std::endl;
-    std::cout<<"  -R = red, Y = yellow, O = orange, B = blue, G = green, W = white"<<std::endl;
-    std::cout<<"  -The cube is to be viewed as if it folds along the"<<std::endl;
-    std::cout<<"  dotted lines INTO the page"<<std::endl;
+    std::cout<<"  -The cube is to be viewed as if it folds along the dotted lines INTO "<<std::endl;
+    std::cout<<"   the screen. If you do not view it this way, some squares may appear"<<std::endl;
+    std::cout<<"   to rotate incorrectly."<<std::endl;
     std::cout<<"        (i.e. face 6 is on the top when folded and face 1" <<std::endl;
     std::cout<<"        is on the bottom when folded)"<<std::endl;
-    std::cout<<"  -The side numbers correlating to cube are shown to the right"<<std::endl;
+    std::cout<<"  -R = red, Y = yellow, O = orange, B = blue, G = green, W = white"<<std::endl;
+    std::cout<<"  -The numbers associated with each side of the cube are shown to the right"<<std::endl;
+    std::cout<<"   of each cube display."<<std::endl;
     std::cout<<""<<std::endl;
     std::cout<<""<<std::endl;
-    std::cout<<"You are ready to begin! Enter a valid number option from the menu below to manipulate the cube:"<<std::endl;
+    std::cout<<"You are ready to begin! Enter a valid number option from the menu below:"<<std::endl;
     std::cout<<""<<std::endl;
 }
 
